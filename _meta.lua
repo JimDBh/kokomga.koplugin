7456653dbd152c50e792d74cd3c8d1f8e9c93205
Jim Davis <jim.jd.davis@gmail.com>
2026-06-21 13:25:41 -0700

    Set version to 0.13

--- _meta.lua
+++ _meta.lua
@@ -1,6 +1,6 @@
 return {
     name = "kokomga",
     fullname = "KOReader Komga client",
-    version = "1.03",
+    version = "0.13",
     description = "Browse, read, and sync progress with your Komga server directly from KOReader.",
 }
