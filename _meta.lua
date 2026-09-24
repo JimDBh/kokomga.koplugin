f6232fce80ee47174c142e5bb072f195710a5dd2
Jim Davis <jim.jd.davis@gmail.com>
2026-09-23 21:17:10 -0700

    Fix version

--- _meta.lua
+++ _meta.lua
@@ -1,6 +1,6 @@
 return {
     name = "kokomga",
     fullname = "KOReader Komga client",
-    version = "2.1.0",
+    version = "2.2.1",
     description = "Browse, read, and sync progress with your Komga server directly from KOReader.",
 }
